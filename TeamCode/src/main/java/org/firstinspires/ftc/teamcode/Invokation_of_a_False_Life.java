d54f7fee2830a3ca53478fcc22ea23d9a63edf55
ricxuuuu <richard.xu3@student.tdsb.on.ca>
2025-12-05 00:20:27 -0500

    further study into the root
    in such cases

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Invokation_of_a_False_Life.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Invokation_of_a_False_Life.java
@@ -2,29 +2,33 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
 
 public class Invokation_of_a_False_Life {
     //create motor, IMU, servo, etc. objects
     public DcMotor frontLeft, frontRight, backLeft, backRight, intake;
     public DcMotorEx flywheel;
-    public IMU imu;
     public Servo hood, flicker;
+    GoBildaPinpointDriver pinpoint;
 
     //create static variables to represent the strings, ease of change.
     private static final String FRONT_LEFT = "frontLeft";
-    private static final String FRONT_RIGHT = "frontRight";
+    private static final String FRONT_RIGHT = "frontRight";         //change these for config file
     private static final String BACK_LEFT = "backLeft";
     private static final String BACK_RIGHT = "backRight";
     private static final String INTAKE = "intake";
     private static final String FLYWHEEL = "flywheel";
     private static final String HOOD = "hood";
     private static final String FLICKER = "flicker";
-    private static final String IMU = "imu";
+    private static final String PINPOINT = "pinpoint";
 
     public void init(HardwareMap hwMap){
         //drivetrain
@@ -32,10 +36,50 @@ public class Invokation_of_a_False_Life {
         frontRight = hwMap.get(DcMotor.class, FRONT_RIGHT);
         backLeft   = hwMap.get(DcMotor.class, BACK_LEFT);
         backRight  = hwMap.get(DcMotor.class, BACK_RIGHT);
-
         //flywheel and intake
         flywheel = hwMap.get(DcMotorEx.class, FLYWHEEL);
         intake = hwMap.get(DcMotor.class, INTAKE);
+        //servos
+        hood = hwMap.get(Servo.class, HOOD);
+        flicker = hwMap.get(Servo.class, FLICKER);
+
+        //IMU
+        pinpoint= hwMap.get(GoBildaPinpointDriver.class, PINPOINT);
+        configurePinpoint();
+        pinpoint.setPosition(new Pose2D(DistanceUnit. INCH, 0, 0, AngleUnit. DEGREES, 0));
+
+        //motor directions
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        flywheel.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+
+        //zero power behaviour
+        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        //flywheel PID
+        flywheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    };
+
+    //--------------------------------------------------------------------------------------------//
+
+    public void configurePinpoint() {
+        //x-off = how left the forward pod is from the tracking point
+        //y-off = how forward the strafe pod is from the tracking point
+        pinpoint.setOffsets(45.166, 64.957, DistanceUnit.MM);
+
+        //set the encoder type to the gobilda 4-arm pods used on #3.
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+
+        //set the directions of the encoders, first the x encoder then the y encoder
+        pinpoint.setEncoderDirections
+                (GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+        //recalibrate, see 'SensorGoBildaPinpoint' for reasoning.
+        pinpoint.resetPosAndIMU();
     };
 
 }
+
